44b3cc3bc8e9e2c245a4aea234efebd1b3db10df
mercury501 <tommaso.desoldaee@gmail.com>
2021-09-27 16:49:59 +0200

    test

--- HelloMavenEclipse/src/main/java/oop21/HelloMavenEclipse/App.java
+++ HelloMavenEclipse/src/main/java/oop21/HelloMavenEclipse/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Maven and Eclipse World!" );
+        System.out.println( "Hello Maven and Eclipse World! OOP21" );
     }
 }
