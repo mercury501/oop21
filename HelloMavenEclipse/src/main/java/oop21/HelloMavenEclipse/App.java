f99ef508730c9e1364ddcc975fe625710dec182d
mercury501 <tommaso.desoldaee@gmail.com>
2021-09-27 16:40:11 +0200

    initial commit

---
+++ HelloMavenEclipse/src/main/java/oop21/HelloMavenEclipse/App.java
@@ -0,0 +1,13 @@
+package oop21.HelloMavenEclipse;
+
+/**
+ * Hello Maven and Exlipse world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Maven and Eclipse World!" );
+    }
+}
