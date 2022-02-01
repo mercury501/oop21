74c20eeb16e3da7989f6eec40e70e8cdd78f28fb
mercury <tommaso.desoldaee@gmail.com>
2022-02-01 18:30:09 +0100

    rendered generate project script os agnostic

--- generate_maven_project.py
+++ generate_maven_project.py
@@ -51,14 +51,10 @@ command = ""
 cd = input("Insert subfolder, leave blank for default, current folder: ")
 
 
-if os.name == "nt":
-	separator = "\\"
-else:
-	separator = "/"
-folder = os.path.dirname(os.path.realpath(__file__)) + separator + cd
-command += "cd " + folder + " &" 
 
-command += "mvn archetype:generate -DgroupId="
+folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), cd)
+
+command += "cd " + folder + " & " + "mvn archetype:generate -DgroupId="
 
 GID = input("Input goupID, leave blank for default(" + default_GID + "): ")
 
@@ -71,22 +67,27 @@ AID = input("Input artifactID, leave blank for default(" + default_AID + "): ")
 
 if AID == "":
 	AID = default_AID
+
 extended_GID = GID + '.' + AID
 command += extended_GID + " -DartifactId="
 command += AID + " -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false"
 
 print("Generating project with groupID: " + GID +", artifactID: " + AID)
 print(command)
+
 os.system(command)
 pom_mod = pom_mod1 + GID + "." + AID + pom_mod2
 
+old_pom_path = os.path.join(folder, AID, "pom.xml")
+new_pom_path = os.path.join(folder, AID, "npom.xml")
+
 print("Adding dependencies to pom.xml...")
 
-with open(folder + AID + separator + 'pom.xml') as f1:
+with open(old_pom_path) as f1:
     lines = f1.readlines()
     f1.close()
 
-with open(folder + AID + separator + 'npom.xml', 'w') as f2:
+with open(new_pom_path, 'w') as f2:
 	f2.writelines(lines[0:16])
 	f2.writelines(pom_mod_lambdaMod)
 	f2.writelines(lines[18:73])
@@ -94,10 +95,11 @@ with open(folder + AID + separator + 'npom.xml', 'w') as f2:
 
 	f2.close()
 
-rm_old_pom = "rm " + folder + AID + separator + "pom.xml"
-rename_pom = "mv "  + folder + AID + separator + "npom.xml " + folder + AID + separator + "pom.xml"
-subprocess.run(["powershell", "-Command", rm_old_pom])
-subprocess.run(["powershell", "-Command", rename_pom])
+os.remove(old_pom_path)
+os.rename(new_pom_path, old_pom_path)
+
+#subprocess.run(["powershell", "-Command", rm_old_pom])
+#subprocess.run(["powershell", "-Command", rename_pom])
 
 print("DONE!")
 #time.sleep(2)
